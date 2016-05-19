f76e61b8e478ce4134254428f5776fcf9ba0940c
Luís Magalhães <lmaga801@outlook.com>
2016-05-19 14:49:30 +0100

    Improved team pics

--- content/js/scripts.js
+++ content/js/scripts.js
@@ -1,43 +1,43 @@
 var team = [
-    ["28_CarlosPereira.gif", "Carlos Pereira", "", "Team A", "Team A"],
-    ["19_ClaudioMonteiro.gif", "Cláudio Monteiro", "", "Team A", "Team A"],
-    ["03_JoaoBandeira.gif", "João Bandeira", "", "Team A", "Team A"],
-    ["05_JoaoCosta.gif", "João Costa", "", "Team A", "Team A"],
-    ["27_JoaoPereira.gif", "João Pereira", "", "Team A", "Team A"],
-    ["29_JoseCardoso.gif", "José Cardoso", "Team Leader", "Team A", "Team A"],
-    ["17_RicardoFigueiredo.gif", "Ricardo Figueiredo", "", "Team A", "Team A"],
+    ["Carlos_Pereira.jpg", "Carlos Pereira", "", "Team A", "Team A"],
+    ["Claudio_Monteiro.jpg", "Cláudio Monteiro", "", "Team A", "Team A"],
+    ["Joao_Bandeira.jpg", "João Bandeira", "", "Team A", "Team A"],
+    ["Joao_Costa.jpg", "João Costa", "", "Team A", "Team A"],
+    ["Joao_Pereira.jpg", "João Pereira", "", "Team A", "Team A"],
+    ["Jose_Cardoso.jpg", "José Cardoso", "Team Leader", "Team A", "Team A"],
+    ["Ricardo_Figueiredo.jpg", "Ricardo Figueiredo", "", "Team A", "Team A"],
 
-    ["30_AnaisDias.gif", "Anaís Dias", "COO - Team Leader", "Team B", "Board Team B"],
-    ["33_DanielCouceiro.gif", "Daniel Couceiro", "", "Team B", "Team B"],
-    ["11_MiguelNunes.gif", "Miguel Nunes", "", "Team B", "Team B"],
-    ["34_JoaoFernandes.gif", "João Fernandes", "", "Team B", "Team B"],
-    ["07_PedroCarreira.gif", "Pedro Carreira", "", "Team B", "Team B"],
-    ["12_RicardoLoureiro.gif", "Ricardo Loureiro", "", "Team B", "Team B"],
-    ["32_RitaLima.gif", "Rita Lima", "", "Team B", "Team B"],
+    ["Anais_Dias.jpg", "Anaís Dias", "COO - Team Leader", "Team B", "Board Team B"],
+    ["Daniel_Couceiro.jpg", "Daniel Couceiro", "", "Team B", "Team B"],
+    ["Miguel_Nunes.jpg", "Miguel Nunes", "", "Team B", "Team B"],
+    ["Joao_Fernandes.jpg", "João Fernandes", "", "Team B", "Team B"],
+    ["Pedro_Carreira.jpg", "Pedro Carreira", "", "Team B", "Team B"],
+    ["Ricardo_Loureiro.jpg", "Ricardo Loureiro", "", "Team B", "Team B"],
+    ["Rita_Lima.jpg", "Rita Lima", "", "Team B", "Team B"],
 
-    ["25_AnaRitaFerreira.gif", "Ana Rita Ferreira", "Team Leader", "Team C", "Team C"],
-    ["01_AndrePires.gif", "André Pires", "", "Team C", "Team C"],
-    ["21_JoaoCardoso.gif", "João Cardoso", "", "Team C", "Team C"],
-    ["08_JoaoSoares.gif", "João Soares", "", "Team C", "Team C"],
-    ["09_LilianaRibeiro.gif", "Liliana Ribeiro", "", "Team C", "Team C"],
-    ["04_LuisMagalhaes.gif", "Luís Magalhães", "CEO", "Team C", "Board Team C"],
+    ["Ana_Rita_Ferreira.jpg", "Ana Rita Ferreira", "Team Leader", "Team C", "Team C"],
+    ["Andre_Pires.jpg", "André Pires", "", "Team C", "Team C"],
+    ["Joao_Cardoso.jpg", "João Cardoso", "", "Team C", "Team C"],
+    ["Joao_Soares.jpg", "João Soares", "", "Team C", "Team C"],
+    ["Liliana_Ribeiro.jpg", "Liliana Ribeiro", "", "Team C", "Team C"],
+    ["Luis_Magalhaes.jpg", "Luís Magalhães", "CEO", "Team C", "Board Team C"],
 
-    ["20_AndreBordalo.gif", "André Bordalo", "", "Team D", "Team D"],
-    ["24_AndreRegado.gif", "André Regado", "", "Team D", "Team D"],
-    ["10_DiogoGomes.gif", "Diogo Gomes", "", "Team D", "Team D"],
-    ["16_MiguelSandim.gif", "Miguel Sandim", "Team Leader", "Team D", "Team D"],
-    ["06_NunoDuarte.gif", "Nuno Duarte", "", "Team D", "Team D"],
-    ["02_VitorTeixeira.gif", "Vítor Teixeira", "", "Team D", "Team D"],
+    ["Andre_Bordalo.jpg", "André Bordalo", "", "Team D", "Team D"],
+    ["Andre_Regado.jpg", "André Regado", "", "Team D", "Team D"],
+    ["Diogo_Gomes.jpg", "Diogo Gomes", "", "Team D", "Team D"],
+    ["Miguel_Sandim.jpg", "Miguel Sandim", "Team Leader", "Team D", "Team D"],
+    ["Nuno_Duarte.jpg", "Nuno Duarte", "", "Team D", "Team D"],
+    ["Vitor_Teixeira.jpg", "Vítor Teixeira", "", "Team D", "Team D"],
 
-    ["13_AnaAlves.gif", "Ana Alves", "", "Multimedia", "Multimedia"],
-    ["15_AndrePeixoto.gif", "André Peixoto", "2nd CMO", "Multimedia", "Board Multimedia"],
-    ["14_BeatrizCavaleiro.gif", "Beatriz Cavaleiro", "CMO", "Multimedia", "Board Multimedia"],
-    ["26_BrunoAlves.gif", "Bruno Alves", "", "Multimedia", "Multimedia"],
-    ["18_Matheus.gif", "Matheus Scarlatti", "2nd CFO", "Multimedia", "Board Multimedia"],
-    ["31_RaquelCorreia.gif", "Raquel Correia", "CFO", "Multimedia", "Board Multimedia"],
+    ["Ana_Alves.jpg", "Ana Alves", "", "Multimedia", "Multimedia"],
+    ["Andre_Peixoto.jpg", "André Peixoto", "Assistant CMO", "Multimedia", "Board Multimedia"],
+    ["Beatriz_Cavaleiro.jpg", "Beatriz Cavaleiro", "CMO", "Multimedia", "Board Multimedia"],
+    ["Bruno_Alves.jpg", "Bruno Alves", "", "Multimedia", "Multimedia"],
+    ["Matheus.jpg", "Matheus Scarlatti", "Assistant CFO", "Multimedia", "Board Multimedia"],
+    ["Raquel_Correia.jpg", "Raquel Correia", "CFO", "Multimedia", "Board Multimedia"],
 
-    ["22_CarolinaFaria.gif", "Carolina Faria", "CCO", "Design", "Board Design"],
-    ["23_Guilherme.gif", "Guilherme", "", "Design", "Design"]
+    ["Carolina_Faria.jpg", "Carolina Faria", "CCO", "Design", "Board Design"],
+    ["Guilherme.jpg", "Guilherme", "", "Design", "Design"]
 ];
 
 $(document).ready(function () {
@@ -78,41 +78,59 @@ function CreateTeamMemberBtns() {
         var teams = info[4];
 
         var outerCol = $(document.createElement('DIV'));
-        var attrClass = 'col col-md-2 col-sm-3 col-xs-4 img-responsive team-member-img-col';
+        var attrClass = 'col col-md-2 col-sm-3 col-xs-3 img-responsive team-member-img-col';
         outerCol.attr('class', attrClass);
         outerCol.attr('team', teams);
 
-        var overlay = $(document.createElement("DIV"));
-        overlay.attr("class", "team-member-overlay");
+        var container = $(document.createElement("DIV"));
+        container.attr("class", "team-member-container");
 
         /* Creating Member Image */
         var img = $(document.createElement("IMG"));
         img.attr('src', 'content/img/team/' + gif);
         img.attr('alt', name);
         img.attr('title', name);
+        img.attr('width', '900');
+        img.attr('height', '900');
         img.attr("class", "team-member-img img-responsive");
 
         var pName = $(document.createElement('P'));
         pName.html(name.length > 0 ? name : 'Name');
         pName.attr('class', 'member-name');
-
         var pPosition = $(document.createElement('p'));
         pPosition.html(position.length ? position : '');
         pPosition.attr('class', 'member-position');
-
         var pTeam = $(document.createElement('P'));
         pTeam.html(visualTeam.length > 0 ? visualTeam : 'Team X');
         pTeam.attr('class', 'member-team');
-        var infoElem = $(document.createElement("DIV"));
-        infoElem.attr('class', 'member-info');
 
-        infoElem.append(pName);
-        infoElem.append(pTeam);
-        infoElem.append(pPosition);
+        var infoWrapper = $(document.createElement("DIV"));
+        infoWrapper.attr('class', 'member-info-wrapper');
+        var infoContainer = $(document.createElement("DIV"));
+        infoContainer.attr('class', 'member-info');
 
-        overlay.append(img);
-        //overlay.append(infoElem);
-        outerCol.append(overlay);
+        infoContainer.append(pName);
+        infoContainer.append(pTeam);
+        infoContainer.append(pPosition);
+        infoWrapper.append(infoContainer);
+        container.append(img);
+        container.append(infoWrapper);
+        outerCol.append(container);
+
+        container.on('mouseover', function() {
+            container.addClass('hover');
+        });
+
+        container.on('click', function() {
+            container.toggleClass('click');
+        });
+
+        container.on('mouseleave', function() {
+            container.removeClass('hover');
+            container.removeClass('click');
+        });
+
+        //infoContainer.hide();
 
         $("#team-pics").append(outerCol);
     });
